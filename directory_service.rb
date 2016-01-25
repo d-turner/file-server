89b5eaeb83b88b92237c22c1658f3b16b6373db0
d-turner <dturner@tcd.ie>
2016-01-25 01:49:09 +0000

    Directory service now gets a listing of all files a server has and returns the corresponding server address if the file exists

--- directory_service.rb
+++ directory_service.rb
@@ -5,7 +5,9 @@ class Directory_Service < Socket_Server
     # call initializer of the server class
     #@port, @server, @max_threads, @max(q), @que, @ip, @main(thread)
     super(port)
-    @file_servers = {'A'=>"4001\n", 'B'=> "4002\n", 'C'=>"4003\n", 'D'=>"4004\n"}
+    @file_servers = {1=>'localhost:4001', 2=>'localhost:4002', 3=>'localhost:4003', 4=>'localhost:4004'}
+    @files = {}
+    query_servers
   end
 
   # override connection
@@ -20,20 +22,22 @@ class Directory_Service < Socket_Server
                 kill(client)
               elsif read_line.start_with?('HELO')
                 student(client, read_line)
-              else
+              elsif read_line.start_with?(@find_server)
                 #elsif read_line.start_with?('open')
-                  # expecting=> "open /dir1/dir2/file.txt"
+                  # expecting=> "open file.txt"
                   #          => "open ComputerName:\dir1\dir2\file.txt"
                   #words = read_line.split(' ')
                   #directory = words[1].split('/')
                   #directory = directory.reject{|d| d.empty?}
-                address = lookup(read_line.split(' ')[1].split('/').reject {|d| d.empty?}[0] )
-                # Either connect to file server and make request or
-                # (doing this one) return the address:port to make a new socket connection
-                #socket = TCPServer.new 'localhost', address
-                if address.nil?; client.sendmsg("\n")
-                else client.sendmsg(address.to_s); end
+                read_line = read_line.strip
+                puts "#{read_line}"
+		            #address = lookup(read_line.split(' ')[1].split('/').reject {|d| d.empty?}[0] )
+                address = lookup(read_line.split(':')[1])
+                if address.nil?; client.puts(@not_found)
+                else client.puts(address); end
                 client.flush
+                client.close
+                break
               end
             end
           end
@@ -47,20 +51,40 @@ class Directory_Service < Socket_Server
     end
   end
 
-  def lookup(dir)
-    @file_servers[dir]
+  def lookup(filename)
+    @files[filename]
   end
 
-  def add_server(name, port)
-    @file_servers[name=>port]
+  def add_server(name, address)
+    @file_servers[name=>address]
   end
 
   def remove_server(server_name)
     @file_servers.delete(server_name)
   end
 
-  def update_server(server_name, new_port )
-    @file_servers[server_name] = new_port
+  def update_server(server_name, new_location )
+    @file_servers[server_name] = new_location
+  end
+
+  def query_servers
+    @file_servers.each do |key, location|
+      local = location.split(':')
+      address = local[0]
+      port = local[1]
+      fs = TCPSocket.new address, port
+      fs.puts(@get_listing)
+      fs.each_line do |read_line|
+        read_line = read_line.strip
+        if read_line == @end_transmission
+          break
+        end
+        filename = read_line.split('/').last
+        @files[filename] = location
+      end
+      fs.close
+    end
+    puts "#{@files}"
   end
 
 end
