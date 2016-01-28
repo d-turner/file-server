4d6c01a7bfa586285eb998bbf8acc9c470d63be1
d-turner <dturner@tcd.ie>
2016-01-28 02:08:38 +0000

    Added code to allow user decide on actions and set the proxy to do the corresponding action

--- client.rb
+++ client.rb
@@ -1,4 +1,61 @@
 require 'socket'
 require './client_proxy'
 proxy  = Client_Proxy.new
-proxy.open('test1a.txt')
+# press 1 to retrieve remote file -> enter file name and press enter
+# press 2 to save local file to remote server -> enter file name and press enter
+# Debugging: press 3 to print files on directory server
+#            press 4 to print servers on directory server
+#            press 5 make directory server query file servers
+# press 6 to exit
+
+def print(x)
+  c = TCPSocket.new 'localhost', 3001
+  c.puts("--ACTION:#{x}\n")
+  c.close
+end
+
+def kill
+  c = TCPSocket.new 'localhost', 3001
+  c.puts("KILL_SERVICE\n")
+  c.close
+end
+
+def helo
+  c = TCPSocket.new 'localhost', 3001
+  c.puts("HELO anything\n")
+  deets = c.readlines
+  puts "#{deets}"
+  c.close
+end
+
+input = 0
+while input != "9\n"
+
+  puts "# Press 1 to retrieve remote file
+# Press 2 to save local file to remote server
+# Debugging: press 3 to print files on directory server
+#            press 4 to print servers on directory server
+#            press 5 make directory server query file servers
+#            press 6 to KILL all servers
+#            press 7 to print HELO message
+# Press 9 to exit\n"
+  input = gets
+  case input
+    when "1\n"
+      puts "Enter file name and press enter\n"
+      filename = gets
+      proxy.open(filename.strip)
+    when "2\n"
+      puts "Enter file name and press enter\n"
+      puts "Nothing yet\n"
+      #filename = gets
+      #proxy.open(filename)
+    when "3\n"; print(1)
+    when "4\n"; print(2)
+    when "5\n"; print(3)
+    when "6\n"; kill
+    when "7\n"; helo
+    when "9\n"; puts puts 'Goodbye'
+    else puts "I don't understand #{input}"
+  end
+end
--- client_proxy.rb
+++ client_proxy.rb
@@ -1,5 +1,16 @@
 require 'socket'
 class Client_Proxy
+  # for the proxy implement the % part
+  # eg OPEN = "--OPEN:%s\n"
+  # puts OPEN % filename
+END_TRANS = "--END--\n"
+NOT_FOUND = "404\n"
+FIND_SERVER = "--WHERE_IS:%s\n"
+OPEN_FILE = "--OPEN:%s\n"
+WRITE_FILE = "--WRITE:%s\n"
+KILL = "KILL_SERVICE\n"
+ACTION = "--ACTION:%d\n"
+
   def initialize
     @dir_server_address = 'localhost'
     @dir_server_port = 3001
@@ -12,43 +23,40 @@ class Client_Proxy
   end
 
   def open(filename)
-    # directory server
-    reply = ds(filename)
-    reply = reply.strip
-    if reply == @not_found
+    reply = directory_server(filename)
+    if reply == NOT_FOUND
       puts 'File does not exist'
     else
-      # file server
-      reply= reply.split(':')
-      address = reply[0]
-      port = reply[1]
-      fs = TCPSocket.new address, port
-      fs.puts(@open_file+filename)
+      address = reply.strip.split(':')
+      ip = address[0]
+      port = address[1]
+      fs = TCPSocket.new ip, port
+      fs.puts(OPEN_FILE % filename)
       save_file(fs, filename)
-      fs.close
     end
   end
 
-  def ds(filename)
+  def directory_server(filename)
     ds = TCPSocket.new @dir_server_address, @dir_server_port
-    ds.puts(@find_server + filename)
+    ds.puts(FIND_SERVER % filename)
     reply = ds.readline
     ds.close
     reply
   end
 
   def save_file(fs, filename)
-    #size = fs.readline
     File.open(@output_dir+filename, 'w') do |file|
       #while chunk = as this may not work
       #fs.read(size) do |chunk|
       #  file.write(chunk)
       #end
       fs.each_line do |line|
-        if line.strip == @end_transmission
-          break
-        end
+        puts line
+        if line == END_TRANS
+          puts 'File saved'
+        else
           file.write(line)
+        end
       end
     end
   end
